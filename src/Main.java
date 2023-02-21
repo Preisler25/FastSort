1b70a6663d99822a53f64d04e15556fefb84b0e6
Preisler <preisler.andras@gmail.com>
2023-02-21 14:05:29 +0100

    working as hell

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        int array[] = {2, 4, 3, 6, 1, 9 ,5 ,8 ,7};
+        quickSort(array, 0, array.length - 1);
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+
+    private static void quickSort(int[] array, int start, int end) {
+        if(start < end) {
+            int partitionIndex = partition(array, start, end);
+            quickSort(array, start, partitionIndex - 1);
+            quickSort(array, partitionIndex + 1, end);
+        }
+    }
+
+    private static int partition(int[] array, int start, int end) {
+        int pivot = array[end];
+        int i = start - 1;
+        for(int j = start; j < end; j++) {
+            if(array[j] <= pivot) {
+                i++;
+                swap(array, i, j);
+            }
+        }
+        swap(array, i + 1, end);
+        return i + 1;
+    }
+
+    private static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 }
